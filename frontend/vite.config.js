01b629e03ff9f20676cc9cb902cc3a110751697e
gyawat.magar <Gyawat.magar@gmail.com>
2025-04-25 14:38:37 +0300

    Add Vite config and fix react-router-dom version

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,15 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    commonjsOptions: {
+      include: [/node_modules/],
+    },
+  },
+  optimizeDeps: {
+    include: ['react-router-dom'],
+  },
 })
